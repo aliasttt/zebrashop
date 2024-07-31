ad17b8e4ef1ffc4a35ce4357d0886cac4d5bd0ec
aliasttt <aliasadi3853@gmail.com>
2024-07-31 12:15:40 +0300

    updating

--- home/forms.py
+++ home/forms.py
@@ -14,3 +14,9 @@ class AccountForm(forms.ModelForm):
         model = Account
         fields = ['name', 'lastname', 'phone', 'password', 'email']
     
+
+class emailform(forms.ModelForm):
+    name = forms.CharField(max_length=30,required=True,label= 'نام')
+    email = forms.EmailField(required=True,label= 'ایمیل')
+    subject = forms.CharField(required=False,max_length=20,label='موضوع')
+    text = forms.CharField( max_length=200, required=True, label ='پیام' )
